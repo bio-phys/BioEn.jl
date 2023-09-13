d205ca36b4cf7f9f84c2ca69b9d4877744aba61b
jkoefinger <juergen.koefinger@biophys.mpg.de>
2023-09-13 13:17:59 +0200

    saving output for convenience

--- src/Output.jl
+++ src/Output.jl
@@ -3,7 +3,26 @@ Writing out results of BioEn refinemnt.
 """
 module Output
 
-import HDF5
+using HDF5 # for compatibility with Python 
+
+function delete(filename)
+    if isfile(filename) 
+        println("Deleting file \"$filename\".")
+        rm(filename)
+    else
+        println("File \"$filename\" does not exist.")
+    end
+end
+
+function write_HDF5(filename; ws, thetas)
+    h5write(filename, "ws", ws)
+    h5write(filename, "thetas", thetas)
+end
+
+function read_HDF5(filename)
+    ws = h5read(filename, "ws")
+    thetas = h5read(filename, "thetas")
+end
 
 
 end # end module Output 
