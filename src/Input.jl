d497a2a5827cf26e07e533356f3dbbd98ff8b106
jkoefinger <juergen.koefinger@biophys.mpg.de>
2023-09-14 09:54:57 +0200

    shape checking and path modification

--- src/Input.jl
+++ src/Input.jl
@@ -9,26 +9,39 @@ module Input
 using DelimitedFiles #, HDF5
 # input
 
+export read_txt_normalized, write_txt_normalized
+
 ## txt files
+
+function modify_path(path)
+    if path[end]!='/'
+        path=path*"/"
+    end
+    return path
+end
 
 function have_consistent_sizes(Y::Vector{T}, y::Array{T, 2}) where T<:AbstractFloat
     MY = size(Y,1)
     My = size(y, 2) 
     MY == My && return true
     #error("Shapes of data (M=$MY) and calculated observables (M=$My) are not consistent!")
-    throw("Shapes of data (M=$MY) and calculated observables (M=$My) are not consistent!")
+    println("Shapes of data (M=$MY) and calculated observables (M=$My) are not consistent!")
     return false
 end
 
-function have_consistent_sizes(y::Array{T,2}, w0::Vector{T}) where T<:AbstractFloat
-    Ny = size(y,1)
-    Nw0 = size(w0, 1)
-    (Ny == Nw0 || w0==[]) && return true
-    throw("Shapes of calculated observables (N=$Ny) and reference weights (N=$Nw0) are not consistent!")
-    return false
+function have_consistent_sizes(y::Array{T,2}, w0) where T<:AbstractFloat
+    if w0 == [] 
+        return true
+    else
+        Ny = size(y,1)
+        Nw0 = size(w0, 1)
+        Ny == Nw0 && return true
+        println("Shapes of calculated observables (N=$Ny) and reference weights (N=$Nw0) are not consistent!")  
+        return false
+    end
 end
 
-function have_consistent_sizes(Y::Vector{T}, y::Array{T,2}, w0::Vector{T}) where T<:AbstractFloat
+function have_consistent_sizes(Y::Vector{T}, y::Array{T,2}, w0) where T<:AbstractFloat
     return have_consistent_sizes(Y, y) && have_consistent_sizes(y, w0) 
 end
 
@@ -47,6 +60,7 @@ end
 function read_txt_normalized(path; Y_name, y_name, w0_name="")
     println("Reading files from path \"$path\".\n")
     println("Reading data from file \"$Y_name\".")
+    path = modify_path(path)
     Y, MY = read_txt_1D(path*Y_name)
     println("Reading calculated observables from file \"$y_name\".")
     y, (Ny, My) = read_txt(path*y_name)
@@ -69,11 +83,10 @@ function write_txt(filename, arr)
 end
 
 function write_txt_normalized(path; Y_name, Y, y_name, y, w0_name="", w0=[])
-    #todo: check shapes before writing
-    try 
-        have_consistent_sizes(Y, y, w0) 
+    if have_consistent_sizes(Y, y, w0) 
         println("Writing files to path \"$path\".\n")
         println("Writing data to file \"$Y_name\".")
+        path = modify_path(path)
         write_txt(path*Y_name, Y)
         println("Writing calculated observables to file \"$y_name\".")
         write_txt(path*y_name, y)
@@ -81,9 +94,8 @@ function write_txt_normalized(path; Y_name, Y, y_name, y, w0_name="", w0=[])
             println("Writing reference weights to file \"$w0_name\".")
             write_txt(path*w0_name, w0)
         end
-    catch ex
+    else
         println("Shapes of arrays are not consistent! No output!")
-        println(ex)
     end
 end
 
