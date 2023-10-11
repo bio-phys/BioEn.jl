70e5e8eac5044f4b681e4e26e1c58e2d00bb7e3a
jkoefinger <juergen.koefinger@biophys.mpg.de>
2023-10-11 11:15:47 +0200

    simple test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,10 @@
 using Test
-include("../../RefinementModels.jl/src/RefinementModels.jl")
 include("../../BioEn.jl/src/BioEn.jl")
-#includet("../src/BioEn.jl")
 
-# Input
-n=50
-Y=[40.]
-M = size(Y)[1]
-sigmas = [3.]
-x = range(1,n)
-
-# Model
-u = RefinementModels.DiscDoubleWell.energy(x)
-p = RefinementModels.DiscDoubleWell.distribution(x)
-
-# Sampling
-N = 5000
-# sample = RefinementModels.DiscDoubleWell.sample(N, x, p)
+N = 3
+M = 1
 y = zeros(N, M);
-y[:,1] =  sample;
+y[:,1] =  [0.2, 0.3, 1.1]
 w0 = ones(N)/N
 g0 = log.(w0)
 # If calculated and measured averages are equal, then first calculating the forces from the reference weights and calculating the weights from thees forces should give the original reference weights (independent of theta)
