3162fc896cfd1eb73bd301e5622e4b970e73b891
jkoefinger <juergen.koefinger@biophys.mpg.de>
2023-10-20 16:20:47 +0200

    updated ouput messages

--- src/Output.jl
+++ src/Output.jl
@@ -7,19 +7,21 @@ using HDF5 # for compatibility with Python
 
 function delete(filename)
     if isfile(filename) 
-        println("Deleting file \"$filename\".")
+        println("\nDeleting file \"$filename\".")
         rm(filename)
     else
-        println("File \"$filename\" does not exist.")
+        println("\nFile \"$filename\" does not exist. No deletion necessary.")
     end
 end
 
 function write_HDF5(filename; ws, thetas)
+    println("\nWriting file \"$filename\".")
     h5write(filename, "ws", ws)
     h5write(filename, "thetas", thetas)
 end
 
 function read_HDF5(filename)
+    println("\nReading file \"$filename\".")
     ws = h5read(filename, "ws")
     thetas = h5read(filename, "thetas")
 end
